3d4def7c0bac8f83fe269349a542bc09857aea52
Karan Kumar <divakarkaran427@gmail.com>
2026-09-21 12:11:56 +0530

    Update BharatTube project

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const mongoose = require("mongoose");
 const app = require("./app");
 
