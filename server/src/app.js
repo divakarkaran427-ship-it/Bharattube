b93d3780218d91f40cfaefafae9f708a5b35db3e
Karan Kumar <divakarkaran427@gmail.com>
2026-09-21 22:29:24 +0530

    Connect frontend to production backend

--- server/src/app.js
+++ server/src/app.js
@@ -114,7 +114,7 @@ app.use(cors({
     "http://localhost",          // Capacitor Android WebView
     "https://bharattube.in",    // Production
     "https://www.bharattube.in",
-    "https://bharattube.vercel.app", // Vercel
+      "https://bharattube-ui-ktnb.vercel.app", // Vercel
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
