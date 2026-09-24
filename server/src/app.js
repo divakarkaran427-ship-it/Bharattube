77e228736d806bb1c8a628a7e83ed0b008029459
Karan Kumar <divakarkaran427@gmail.com>
2026-09-24 19:10:36 +0530

    Update BharatTube

--- server/src/app.js
+++ server/src/app.js
@@ -114,7 +114,7 @@ app.use(cors({
     "http://localhost",          // Capacitor Android WebView
     "https://bharattube.in",    // Production
     "https://www.bharattube.in",
-      "https://bharattube-rouge.vercel.app", // Vercel
+      "https://bharattube-wubg.vercel.app", // Vercel
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
